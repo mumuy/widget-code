9663d9d0ee167388e776db32444f0a447e44f7b6
zhenghaole <89932980@qq.com>
2024-05-27 14:28:56 +0800

    updated 复制方法更新

--- src/module/method/copyToClipboard.js
+++ src/module/method/copyToClipboard.js
@@ -1,22 +1,26 @@
 export default function(text){
-    return new Promise((resolve,reject)=>{
-        if(document.execCommand){
-            window.getSelection().removeAllRanges();
-        	var selection = window.getSelection();
-            var $dom = document.createElement('pre');
-            $dom.style.position = 'absolute';
-            $dom.style.left = '-99999px';
-            document.body.appendChild($dom);
-            $dom.innerText = text;
-            $dom.focus();
-            selection.selectAllChildren($dom);
-        	document.execCommand("Copy");
-            window.setTimeout(function () {
-                document.body.removeChild($dom);
-                resolve();
-            }, 100);
-        }else{
-            reject();
-        }
-    });
+    if(navigator.clipboard){
+        return navigator.clipboard.writeText(text);
+    }else{
+        return new Promise((resolve,reject)=>{
+            if(document.execCommand){
+                window.getSelection().removeAllRanges();
+                var selection = window.getSelection();
+                var $dom = document.createElement('pre');
+                $dom.style.position = 'absolute';
+                $dom.style.left = '-99999px';
+                document.body.appendChild($dom);
+                $dom.innerText = text;
+                $dom.focus();
+                selection.selectAllChildren($dom);
+                document.execCommand("Copy");
+                window.setTimeout(function () {
+                    document.body.removeChild($dom);
+                    resolve();
+                }, 100);
+            }else{
+                reject();
+            }
+        });
+    }
 };
